1255f9e5204e930505e8f85244ce83d62603d6cf
Ricardo Urbano <ricardo.urbano@SEDE.CEJAM>
2025-08-07 12:45:37 -0300

    criando migrations do banco

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RastreamentoController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', HomeController::class);
+Route::get('/rastreamento', RastreamentoController::class);
